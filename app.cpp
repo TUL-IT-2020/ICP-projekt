28f65c4341ef416b07179f1429d5f6ad86b50050
elPytel <jaroslav.korner1@gmail.com>
2025-05-21 14:30:52 +0200

    opravené pořadí vykreslovaných pruhledných objektů.

--- app.cpp
+++ app.cpp
@@ -708,11 +708,9 @@ int App::run(void) {
 			}
 
 			// SECOND PART - draw only transparent - painter's algorithm (sort by distance from camera, from far to near)
-            std::sort(transparent.begin(), transparent.end(), [&](Model const * a, Model const * b) {
-                glm::vec3 translation_a = glm::vec3(a->local_model_matrix[3]);  // get 3 values from last column of model matrix = translation
-                glm::vec3 translation_b = glm::vec3(b->local_model_matrix[3]);  // dtto for model B
-                return glm::distance(camera.Position, translation_a) < glm::distance(camera.Position, translation_b); // sort by distance from camera
-                });
+			std::sort(transparent.begin(), transparent.end(), [&](Model const * a, Model const * b) {
+				return glm::distance(camera.Position, a->origin) > glm::distance(camera.Position, b->origin);
+			});
 
             // set GL for transparent objects
             glEnable(GL_BLEND);
